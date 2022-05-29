ae179ccc5d6799ebb4ff8290f6c2f41a004a3eed
GaussianSums <99705187+C14H14N2O4@users.noreply.github.com>
2022-05-28 19:09:39 -0700

    connected to express

--- jeopardy_client/src/routes/Button.js
+++ jeopardy_client/src/routes/Button.js
@@ -1,11 +1,22 @@
 import { useLocation } from "react-router";
+import axios from "axios"
 
 export default function Button() {
     const {state} = useLocation()
     const {user} = state;
+    const buzzer = () => {
+        fetch("http://localhost:5000", {
+            mode: 'no-cors'
+        })
+        .then(res => res)
+        .then(
+            (result) => {
+            console.log(result);
+        });
+    }
     return (
         <div> 
-            {user}
+            <button onClick = {buzzer}>Buzz</button>
         </div>
     );
 }
--- jeopardy_server/server.js
+++ jeopardy_server/server.js
@@ -4,3 +4,17 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.get('/', (req, res) => {
+    res.send({'Hello World!':[{"test":"test"}]})
+    console.log("got a get req")
+})
+
+app.post('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.put('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
