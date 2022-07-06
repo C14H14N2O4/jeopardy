6ad3c86d0e7c133e472a391dba69903d557e37dc
GaussianSums <99705187+C14H14N2O4@users.noreply.github.com>
2022-07-05 23:27:43 -0700

    testing websocket

--- jeopardy_client/src/components/WebTest.js
+++ jeopardy_client/src/components/WebTest.js
@@ -1,20 +1,29 @@
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { connection, w3cwebsocket as W3CWebSocket } from 'websocket';
 import { useEffect } from 'react';
 
 export default function WebTest() {
+  
   const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+
+
   useEffect(() => {
     client.onopen = () => {
       console.log('WebSocket Client Connected');
     };
     client.onmessage = (message) => {
-      console.log(message);
+      console.log(message.data);
     };
   })
+
+  const testBuzz = () => {
+    client.send("test")
+  }
 
 
   return (
     <div>
+      <button onClick = {testBuzz}>Buzz</button> 
       Test
     </div>
   )
--- jeopardy_server/server2.js
+++ jeopardy_server/server2.js
@@ -3,22 +3,21 @@ const webSocketServer = require('websocket').server;
 const http = require('http');
 const server = http.createServer();
 server.listen(webSocketsServerPort);
+
+var users = []
+
 const wsServer = new webSocketServer({
   httpServer: server
 });
 
-// const clients = {};
-
-// const getUniqueID = () => {
-//   const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-//   return s4() + s4() + '-' + s4();
-// };
 
 wsServer.on('request', function(request) {
   var d = new Date();
-  // var userID = getUniqueID();
   const connection = request.accept(null, request.origin);
-  // clients[userID] = connection;
-  console.log(connection)
+  connection.on('message', function(message) {
+    console.log(message);
+    connection.send('testMessage')
+  })
   console.log(d);  
 })
+
